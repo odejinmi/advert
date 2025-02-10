350cda0987f92c7e3fa6224366adbc07e675ce98
videx <45696129+odejinmi@users.noreply.github.com>
2025-02-10 10:57:34 +0100

    new advert update

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import advert
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AdvertPlugin.register(with: registry.registrar(forPlugin: "AdvertPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
